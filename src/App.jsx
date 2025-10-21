f6492ce3993d63ef7c6fa97c3f44977d779a07b8
DanteLeandroAguilar <daguilar@uade.edu.ar>
2025-10-21 16:16:20 -0300

    Se corrige toastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { AppRoutes } from './routes/AppRoutes';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  return <AppRoutes />;
-  return <ToastContainer />;
+  return (
+    <>
+      <AppRoutes />
+      <ToastContainer />
+    </>
+  );
 }
 
 export default App;
